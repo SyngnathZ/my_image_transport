77dcef38d62b36ecb9b00d56d764d28af6bc3ed1
SyngnathZ <Hei.jinghui@gmail.com>
2021-07-05 16:51:19 +0800

    Add sub and pub in one node. Node1 -> Node2 -> Node3

--- src/my_subscriber.cpp
+++ src/my_subscriber.cpp
@@ -1,28 +1,38 @@
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
-#include <cv_bridge/cv_bridge.h>
+#include <cv_bridge/cv_bridge.h>s
 
-void imageCallback(const sensor_msgs::ImageConstPtr& msg)
-{
-    try
-    {
-        cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
+image_transport::Subscriber sub;
+image_transport::Publisher pub;
+
+void imageCallback(const sensor_msgs::ImageConstPtr &msg) {
+    try {
+        cv::Mat img, gray;
+        img = cv_bridge::toCvShare(msg, "bgr8")->image;
+        cv::cvtColor(img, gray, CV_BGR2GRAY);
+
+        ros::NodeHandle nh;
+        image_transport::ImageTransport it(nh);
+        sensor_msgs::ImagePtr msg2 = cv_bridge::CvImage(std_msgs::Header(), "mono8", gray).toImageMsg();
+        pub.publish(msg2);
+
+        cv::imshow("view", img);
     }
-    catch (cv_bridge::Exception& e)
-    {
+    catch (cv_bridge::Exception &e) {
         ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
     }
 }
 
-int main(int argc, char **argv)
-{
-    ros::init(argc, argv, "image_listener");
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "image_listener1");
     ros::NodeHandle nh;
     cv::namedWindow("view");
     cv::startWindowThread();
     image_transport::ImageTransport it(nh);
-    image_transport::Subscriber sub = it.subscribe("camera/image", 1, imageCallback);
+    sub = it.subscribe("camera/image", 1, imageCallback);
+    pub = it.advertise("camera/image_gray", 1);
+
     ros::spin();
     cv::destroyWindow("view");
 }
